6b4f6db8fc71e03c1cf9faa41e54f57170ef5b97
ilyasemov <115580012+ilyasemov@users.noreply.github.com>
2023-05-26 00:10:52 +0300

    Create main.cpp

---
+++ 1_1./main.cpp
@@ -0,0 +1,103 @@
+#include <UnitTest++/UnitTest++.h>
+#include <modAlphaCipher.h>
+
+SUITE(KeyTest)
+{
+    TEST(ValidKey) {
+        CHECK_EQUAL("БВГБВ",modAlphaCipher("БВГ").encrypt("ААААА"));
+    }
+    TEST(LongKey) {
+        CHECK_EQUAL("БВГДЕ",modAlphaCipher("БВГДЕЁЖЗИЙ").encrypt("ААААА"));
+    }
+    TEST(LowCaseKey) {
+        CHECK_EQUAL("БВГБВ",modAlphaCipher("бвг").encrypt("ААААА"));
+    }
+    TEST(DigitsInKey) {
+        CHECK_THROW(modAlphaCipher cp("Б1"),cipher_error);
+    }
+    TEST(PunctuationInKey) {
+        CHECK_THROW(modAlphaCipher cp("Б,В"),cipher_error);
+    }
+    TEST(WhitespaceInKey) {
+        CHECK_THROW(modAlphaCipher cp("Б В"),cipher_error);
+    }
+    TEST(EmptyKey) {
+        CHECK_THROW(modAlphaCipher cp(""),cipher_error);
+    }
+    TEST(WeakKey) {
+        CHECK_THROW(modAlphaCipher cp("ААА"),cipher_error);
+    }
+}
+
+struct KeyB_fixture {
+    modAlphaCipher * p;
+    KeyB_fixture()
+    {
+        p = new modAlphaCipher("Б");
+    }
+    ~KeyB_fixture()
+    {
+        delete p;
+    }
+};
+
+SUITE(EncryptTest)
+{
+    TEST_FIXTURE(KeyB_fixture, UpCaseString) {
+        CHECK_EQUAL("ТЁНПГЙМЭА",
+                    p->encrypt("СЕМОВИЛЬЯ"));
+    }
+    TEST_FIXTURE(KeyB_fixture, LowCaseString) {
+        CHECK_EQUAL("ТЁНПГЙМЭАТЁСДЁЁГЙШ",
+                    p->encrypt("СЕМОВИЛЬЯСЕРГЕЕВИЧ"));
+    }
+    TEST_FIXTURE(KeyB_fixture, StringWithWhitspaceAndPunct) {
+        CHECK_EQUAL("ТЁНПГЙМЭАТЁСДЁЁГЙШ",
+                    p->encrypt("СЕМОВИЛЬЯСЕРГЕЕВИЧ ???"));
+    }
+    TEST_FIXTURE(KeyB_fixture, StringWithNumbers) {
+        CHECK_EQUAL("ДПЕБСПЗЕЁОЙА", p->encrypt("2003 года рождения"));
+    }
+    TEST_FIXTURE(KeyB_fixture, EmptyString) {
+        CHECK_THROW(p->encrypt(""),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, NoAlphaString) {
+        CHECK_THROW(p->encrypt("12131+11=1231414"),cipher_error);
+    }
+    TEST(MaxShiftKey) {
+        CHECK_EQUAL("РДЛНБЗКЫЮ",
+                    modAlphaCipher("Я").encrypt("СЕМОВ ИЛЬЯ"));
+    }
+}
+
+SUITE(DecryptText)
+{
+    TEST_FIXTURE(KeyB_fixture, UpCaseString) {
+        CHECK_EQUAL("СЕМОВИЛЬЯСЕРГЕЕВИЧ",
+                    p->decrypt("ТЁНПГЙМЭАТЁСДЁЁГЙШ"));
+    }
+    TEST_FIXTURE(KeyB_fixture, LowCaseString) {
+        CHECK_THROW(p->decrypt("тёнпгймэаТЁСДЁЁГЙШ"),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, WhitespaceString) {
+        CHECK_THROW(p->decrypt("ТЁНПГ ЙМЭА ТЁСДЁЁГЙШ"),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, DigitsString) {
+        CHECK_THROW(p->decrypt("2003ДПЕБСПЗЕЁОЙА"),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, PunctString) {
+        CHECK_THROW(p->decrypt("ДПЕБ,КСПЗЕЁОЙА"),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, EmptyString) {
+        CHECK_THROW(p->decrypt(""),cipher_error);
+    }
+    TEST(MaxShiftKey) {
+        CHECK_EQUAL("БЫСТРАЯБУРАЯЛИСА",
+                    modAlphaCipher("Я").decrypt("АЪРСПЯЮАТПЯЮКЗРЯ"));
+    }
+}
+
+int main(int argc, char **argv)
+{
+    return UnitTest::RunAllTests();
+}
